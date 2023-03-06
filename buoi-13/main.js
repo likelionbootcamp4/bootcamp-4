52ec5d91997e06996a671046759a5188ecac0ea2
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-03-06 14:22:36 +0700

    buoi 13

---
+++ buoi-13/main.js
@@ -0,0 +1,94 @@
+// var primaryColor = "#333";
+// var myName = "Huy";
+// var myAge = 40;
+
+// var myExerciseGrade = 7.5;
+
+// Declare and assign variables hasDisountVoucher to true
+
+// var hasDiscountVoucher = true;
+// var hasDiscountVoucher;
+// hasDiscountVoucher = true;
+
+// console.log(myName);
+// console.log(primaryColor);
+// console.log(hasDiscountVoucher);
+
+// console.log(10 - 5)
+// console.log(10 + 5)
+// console.log(10 / 3)
+// console.log(10 * 3)
+// console.log(10 % 2)
+// console.log(10 ** 2)
+
+// var mathScore = 9;
+// var physicsScore = 8;
+// var chemistryScore = 6;
+
+// var totalScore = mathScore + physicsScore + chemistryScore;
+// var averageScore = totalScore / 3;
+
+console.log(120 > 100); // true
+console.log(120 < 100); // false
+console.log(100 < 100); // false
+console.log(120 == 100); // false
+console.log(120 >= 100); // true
+console.log(100 >= 100); // true
+console.log(120 <= 100); // false
+console.log(100 <= 100); // true
+console.log(100 != 100); // false
+
+console.log(100 === 100); // true
+console.log(100 == 100); // true
+
+console.log("100" === 100); // false
+console.log("100" == 100); // true
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && false); // false
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log(!false); // true
+console.log(!true); // false
+
+var x = 10;
+
+console.log(x);
+
+x += 5; // x = x + 5
+x *= 5; // x = x * 5
+x++; // x += 1 or x = x + 1
+x--; // x -= 1 or x = x - 1
+
+// console.log(10 + 20); // 30
+
+// var yourName = prompt("What is your name?");
+
+// Show dialog and input your name
+// var yourName = prompt("What is your name?");
+
+// Show dialog and input your age
+// var yourAge = prompt("How old are you?");
+
+// Log to console
+// console.log("Hello " + yourName + ", " + yourAge + " years old!");
+
+// backtick (``)
+// interpolate ${__variable__}
+// console.log(`Hello ${yourName}, ${yourAge} years old`);
+
+var myName = "Duc Huy";
+
+// Get number of characters in string
+// console.log(myName.length);
+
+// Lowercase string
+console.log(myName.toLowerCase()); // "duc huy"
+console.log(myName.toUpperCase()); // "DUC HUY"
+
+console.log(myName); // "Duc Huy"
