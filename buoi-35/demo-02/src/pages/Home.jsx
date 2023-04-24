89aa05985d7a0cb7f99f3646ca7a33303bf11dbf
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-04-24 20:00:57 +0700

    buoi 35

--- buoi-35/demo-02/src/pages/Home.jsx
+++ buoi-35/demo-02/src/pages/Home.jsx
@@ -3,5 +3,12 @@ import Navbar from "../components/Navbar";
 import RootLayout from "../components/RootLayout";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <h1>
+      <img
+        src="https://lnfdkaooikvjxeghvcba.supabase.co/storage/v1/object/public/ecommerce/laptops/gearvn-laptop-msi-modern-14-c7m-083vn-2_edaf0908a4984433a1754c8d8a503109.webp"
+        alt=""
+      />
+    </h1>
+  );
 }
