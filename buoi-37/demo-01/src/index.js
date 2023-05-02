ba5a3ecbb97acd48e8549a4d0e5c6075f970c4ed
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-05-02 16:51:13 +0700

    buoi 37

--- buoi-37/demo-01/src/index.js
+++ buoi-37/demo-01/src/index.js
@@ -18,6 +18,9 @@ app.listen(4000, () => {
   console.log("Server is running...");
 });
 
+// Body parsing
+app.use(express.json());
+
 // Handle requests
 // Method: GET
 // Path: /
@@ -36,6 +39,31 @@ app.get("/products", async (req, res) => {
   res.json(products);
 });
 
+// Search product
+// Method: GET
+// Path: /products/search
+app.get("/products/search", async (req, res) => {
+  // Get query q from request
+  const q = req.query.q; // Mobile
+  console.log(q);
+
+  // Check if q exists or not, if not return []
+  if (!q) {
+    return res.json([]);
+  }
+
+  // Get documents based on q
+  // Find all titles contains "laptop" (q)
+  const products = await Product.find({
+    title: new RegExp(q, "i"), // insensitive
+  });
+
+  // Cach 2: title: { $regex: q, $options: "i" }
+
+  // Response to client
+  res.json(products);
+});
+
 // Get a product by id
 // Method: GET
 // Path: /products/:productId
@@ -45,6 +73,53 @@ app.get("/products/:productId", async (req, res) => {
 
   // Find document from "products" collection by id
   const product = await Product.findById(productId);
+
+  // Response to client
+  res.json(product);
+});
+
+// Add a product
+// Method: POST
+// Path: /products/add
+app.post("/products/add", async (req, res) => {
+  // Get body from request
+  const newProduct = req.body;
+
+  // Insert into "products" collection
+  const product = await Product.create(newProduct);
+
+  // Response to client
+  res.json(product);
+});
+
+// Update a product
+// Method: PUT
+// Path: /products/:productId
+app.put("/products/:productId", async (req, res) => {
+  // Get productId from request
+  const productId = req.params.productId;
+
+  // Get body (updateProduct) from request
+  const updateProduct = req.body;
+
+  // Update document based on productId and updateProduct
+  const product = await Product.findByIdAndUpdate(productId, updateProduct, {
+    new: true,
+  });
+
+  // Response to client
+  res.json(product);
+});
+
+// Delete a product by Id
+// Method: DELETE
+// Path: /products/:productId
+app.delete("/products/:productId", async (req, res) => {
+  // Get productId from request
+  const productId = req.params.productId;
+
+  // Delete document based on productId
+  const product = await Product.findByIdAndDelete(productId);
 
   // Response to client
   res.json(product);
