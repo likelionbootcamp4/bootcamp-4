0f34a54eabd359301fda3598b58aa168cdf741de
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-05-04 12:28:29 +0700

    buoi 37

--- buoi-37/server/src/index.js
+++ buoi-37/server/src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Product = require("./models/Product");
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
+const verify = require("./middlewares/verify");
+const jwt = require("jsonwebtoken");
 
 const DB_URI =
   "mongodb+srv://likelionbootcamp4:1111@cluster0.3nn4yog.mongodb.net/ecommerce?retryWrites=true&w=majority";
@@ -25,10 +29,16 @@ app.use(cors());
 // Body parsing
 app.use(express.json());
 
+// Custom middleware
+// app.use((req, res, next) => {
+//   console.log("Authenticate user");
+//   next();
+// });
+
 // Handle requests
 // Method: GET
 // Path: /
-app.get("/", (req, res) => {
+app.get("/", verify, (req, res) => {
   res.json({ message: "Hello!" });
 });
 
@@ -71,29 +81,37 @@ app.get("/products/search", async (req, res) => {
 // Get a product by id
 // Method: GET
 // Path: /products/:productId
-app.get("/products/:productId", async (req, res) => {
-  // Get productId from request
-  const productId = req.params.productId;
+app.get("/products/:productId", async (req, res, next) => {
+  try {
+    // Get productId from request
+    const productId = req.params.productId;
 
-  // Find document from "products" collection by id
-  const product = await Product.findById(productId);
+    // Find document from "products" collection by id
+    const product = await Product.findById(productId);
 
-  // Response to client
-  res.json(product);
+    // Response to client
+    res.json(product);
+  } catch (err) {
+    next(new Error("Product not found!"));
+  }
 });
 
 // Add a product
 // Method: POST
 // Path: /products/add
 app.post("/products/add", async (req, res) => {
-  // Get body from request
-  const newProduct = req.body;
+  try {
+    // Get body from request
+    const newProduct = req.body;
 
-  // Insert into "products" collection
-  const product = await Product.create(newProduct);
+    // Insert into "products" collection
+    const product = await Product.create(newProduct);
 
-  // Response to client
-  res.json(product);
+    // Response to client
+    res.json(product);
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 });
 
 // Update a product
@@ -127,4 +145,97 @@ app.delete("/products/:productId", async (req, res) => {
 
   // Response to client
   res.json(product);
+});
+
+// Sign up
+// Method: POST
+// Path: /signup
+// Body: {username, password}
+app.post("/signup", async (req, res, next) => {
+  try {
+    // Get username and password from body
+    const { username, password } = req.body;
+
+    // Validate username and password
+    if (!username || !password) {
+      throw new Error("Invalid username or passwords");
+    }
+
+    // Check user exists or not
+    const user = await User.findOne({ username });
+    if (user) {
+      throw new Error("Username exists");
+    }
+
+    // Hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    // Create user document in database
+    await User.create({ username, hash });
+
+    // Response to client
+    res.json({ message: "OK" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Sign in
+// Method: POST
+// Path: /signin
+// Body: {username, password}
+app.post("/signin", async (req, res, next) => {
+  try {
+    // Get username and password from body
+    const { username, password } = req.body;
+
+    // Validate username and password
+    if (!username || !password) {
+      throw new Error("Invalid username or password");
+    }
+
+    // Check username exists or not
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Compare password and hash password
+    const isValid = bcrypt.compareSync(password, user.hash);
+
+    // Check valid password
+    if (!isValid) {
+      throw new Error("Invalid password");
+    }
+
+    // Generate access token
+    const accessToken = jwt.sign({ user_id: user._id }, "bimat", {
+      expiresIn: "30s",
+    });
+
+    // Response to client
+    res.json({ message: "OK", accessToken: accessToken });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get all users
+// Method: GET
+// Path: /users
+app.get("/users", async (req, res) => {
+  // Get all users from database
+  const users = await User.find();
+
+  // Response to client
+  res.json(users);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.json({
+    error: err.message || "Page not found",
+  });
 });
